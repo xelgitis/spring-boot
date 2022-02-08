a2c76916138f1624cf9731edd5485190063f1492
Olga Jesic <ojesic@yahoo.com>
2022-02-08 14:14:33 +0100

    adding of resource file

--- src/main/java/com/example/springboot/exeption/ControllerAdvisor.java
+++ src/main/java/com/example/springboot/exeption/ControllerAdvisor.java
@@ -3,6 +3,7 @@ package com.example.springboot.exeption;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
@@ -21,7 +22,10 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 	public ResponseEntity<Object> handleException(VacationAppException ex, WebRequest request) {
 		Map<String, Object> body = new LinkedHashMap<>();
 		
-		body.put("message", setMessage(ex.getStatus()));	
+		ResourceBundle bundle = ResourceBundle.getBundle("messages");
+		String message = bundle.getString(ex.getStatus().jsonValue());
+		
+		body.put("message", message);	
 		body.put("status",  ex.getStatus());
 
 		return new ResponseEntity<>(body, ex.getStatus().getHttpStatus());
@@ -46,18 +50,4 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }	
-    
-    public String setMessage(Status tmp) {    	
-    	switch (tmp) {
-    	    case SUCCESS:              return "uspesna obrada";
-    	    case USERNAME_TAKEN:       return "korinsicko ime vec postoji u bazi";
-    	    case EMAIL_TAKEN   :       return "korisnik sa ovim email-om vec postoji u bazi";    	    
-    	    case GENERIC_ERROR:        return "greska prilikom obrade zahteva - ulogovanom korisniku nije dozvoljeno da kreira-gleda-azurira-brise podatke za drugog korisnika";
-    	    case USER_NOT_FOUND:       return "korisnik sa ovim username-om ne postoji u bazi";
-    	    case WRONG_PASSWORD:       return "korisnik je uneo pogresnu sifru";
-    	    case WRONG_FORMAT_DATA:    return "korisnik je uneo nevalidne podatke";
-    	    case VACATION_NOT_PRESENT: return "Odmor za korisnika ne postoji u bazi"; 
-    		default:                   return "generic error";
-    	}	
-    }
 }
